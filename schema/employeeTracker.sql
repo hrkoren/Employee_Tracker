3097d389382975c00531ee28f1cd35e1360cf325
HRush <hrkoren@gmail.com>
2021-04-11 19:33:36 -0400

    created sql schema code to create db and tables

---
+++ schema/employeeTracker.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    role_id INT AUTO_INCREMENT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title varchar(30),
+    salary decimal,
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    name varchar(30),
+    PRIMARY KEY (id)
+);
